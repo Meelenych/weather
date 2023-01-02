f8cc41fa55ff701790b8cc4223fd098ce99343f3
Meelenych <meelenych@gmail.com>
2023-01-02 16:30:04 +0100

    Colors fix

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -61,48 +61,53 @@ export default function Application() {
 	}, [submitValue]);
 
 	return (
-		<div className='container mx-auto p-4 list-none'>
-			<form onSubmit={handleSubmit}>
-				<button
-					type='submit'
-					className='mr-4 border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold bg-indigo-600 text-white'>
-					<span>Search</span>
-				</button>
+		<div
+			className={
+				cityWeather?.current.cloud > 50 ? 'bg-neutral-300' : 'bg-blue-300'
+			}>
+			<div className='container mx-auto p-4 list-none'>
+				<form onSubmit={handleSubmit}>
+					<button
+						type='submit'
+						className='mr-4 border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold bg-indigo-600 text-white'>
+						<span>Search</span>
+					</button>
 
-				<input
-					type='text'
-					autoComplete='off'
-					placeholder='Search a city'
-					onChange={handleChange}
-					value={changeValue}
-					className='border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold'
-				/>
-
-				<button
-					onClick={addCity}
-					type='button'
-					className='ml-4 border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold bg-indigo-600 text-white'>
-					<span>Add city</span>
-				</button>
-			</form>
-
-			{submitValue && (
-				<WeatherPanel
-					cityWeather={cityWeather}
-					unpinBtn={false}
-				/>
-			)}
-
-			<ul className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-				{citiesWeather.map(element => (
-					<WeatherPanel
-						key={uid()}
-						className='basis-1/4'
-						cityWeather={element}
-						unpin={unpin}
+					<input
+						type='text'
+						autoComplete='off'
+						placeholder='Search a city'
+						onChange={handleChange}
+						value={changeValue}
+						className='border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold'
 					/>
-				))}
-			</ul>
+
+					<button
+						onClick={addCity}
+						type='button'
+						className='ml-4 border-solid border-2 border-indigo-600 p-2 rounded-md text-indigo-800 text-xl font-semiboldfont-semibold bg-indigo-600 text-white'>
+						<span>Add city</span>
+					</button>
+				</form>
+
+				{submitValue && (
+					<WeatherPanel
+						cityWeather={cityWeather}
+						unpinBtn={false}
+					/>
+				)}
+
+				<ul className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+					{citiesWeather.map(element => (
+						<WeatherPanel
+							key={uid()}
+							className='basis-1/4'
+							cityWeather={element}
+							unpin={unpin}
+						/>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 }
--- src/app/weatherPanel/weatherPanel.jsx
+++ src/app/weatherPanel/weatherPanel.jsx
@@ -3,37 +3,46 @@ import PropTypes from 'prop-types';
 
 export default function WeatherPanel({ cityWeather, unpin, unpinBtn }) {
 	return (
-		<li className='bg-indigo-100 rounded-md shadow-xl p-3 w-1/4 mt-4 w-full'>
-			<p className='text-xl font-medium text-indigo-700 '>
-				Country: {cityWeather?.location.country}
-			</p>
-			<p className='text-indigo-400 font-medium'>
-				Region: {cityWeather?.location.region}
-			</p>
-			<p className='text-indigo-400 font-medium'>
-				City: {cityWeather?.location.name}
-			</p>
-			<p>
-				Feels like: {cityWeather?.current.feelslike_c} <span>&#176;</span>C
-			</p>
-			<p>
-				Temperature: {cityWeather?.current.temp_c} <span>&#176;</span>C
-			</p>
-			<img
-				src={
-					cityWeather ? `http://${cityWeather?.current.condition.icon.slice(2)}` : ''
-				}
-			/>
-			<p>Local time: {cityWeather?.location.localtime}</p>
+		<li className='w-1/4 mt-4 w-full'>
+			<div
+				className={
+					cityWeather?.current.is_day === 0
+						? 'bg-blue-900 p-3 rounded-md shadow-xl'
+						: 'bg-blue-400 p-3 rounded-md shadow-xl'
+				}>
+				<p className='text-xl font-medium text-indigo-700 '>
+					Country: {cityWeather?.location.country}
+				</p>
+				<p className='text-indigo-700 font-medium'>
+					Region: {cityWeather?.location.region}
+				</p>
+				<p className='text-indigo-700 font-medium'>
+					City: {cityWeather?.location.name}
+				</p>
+				<p>
+					Feels like: {cityWeather?.current.feelslike_c} <span>&#176;</span>C
+				</p>
+				<p>
+					Temperature: {cityWeather?.current.temp_c} <span>&#176;</span>C
+				</p>
+				<img
+					src={
+						cityWeather
+							? `http://${cityWeather?.current.condition.icon.slice(2)}`
+							: ''
+					}
+				/>
+				<p>Local time: {cityWeather?.location.localtime.slice(10)}</p>
 
-			{unpinBtn !== false && (
-				<button
-					type='button'
-					onClick={unpin}
-					className='border-solid border-2 border-indigo-600 p-1 rounded-md text-lg font-semiboldfont-semibold bg-indigo-600 text-white'>
-					Unpin
-				</button>
-			)}
+				{unpinBtn !== false && (
+					<button
+						type='button'
+						onClick={unpin}
+						className='border-solid border-2 border-indigo-600 p-1 rounded-md text-lg font-semiboldfont-semibold bg-indigo-600 text-white'>
+						Unpin
+					</button>
+				)}
+			</div>
 		</li>
 	);
 }
