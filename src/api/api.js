4c922baa7507e81b0466fc2c2c309c8fb0551463
Meelenych <meelenych@gmail.com>
2024-01-18 15:31:40 -0700

    api protocol change

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 const APIkey = '?key=f0e72a5b37ab461296383508221612';
 
-const baseURL = 'http://api.weatherapi.com/v1';
+const baseURL = 'https://api.weatherapi.com/v1';
 
 const parameters = '/current.json';
 
