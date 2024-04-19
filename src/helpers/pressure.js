bade87230c8a2254681764866c8711c88dbf5f78
Meelenych <meelenych@gmail.com>
2024-04-19 15:32:07 -0600

    pressure helper fix

--- src/helpers/pressure.js
+++ src/helpers/pressure.js
@@ -1,17 +1,17 @@
 export const pressureScale = pressure => {
-	if (pressure >= 28.8 && pressure < 29.39) {
+	if (pressure >= 28.8 && pressure <= 29.39) {
 		return 'very low';
 	}
-	if (pressure >= 29.4 && pressure < 29.79) {
+	if (pressure >= 29.4 && pressure <= 29.79) {
 		return 'low';
 	}
-	if (pressure >= 29.8 && pressure < 30.19) {
+	if (pressure >= 29.8 && pressure <= 30.19) {
 		return 'normal';
 	}
-	if (pressure >= 30.2 && pressure < 30.49) {
+	if (pressure >= 30.2 && pressure <= 30.49) {
 		return 'high';
 	}
-	if (pressure >= 30.5 && pressure < 31) {
+	if (pressure >= 30.5 && pressure <= 31) {
 		return 'very high';
 	}
 };
